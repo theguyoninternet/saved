4a2da1071a39c16f275540e91501cfa2a5594e60
danielf.s1555 <108942528+theguyoninternet@users.noreply.github.com>
2022-11-09 14:55:37 -0300

    Update test.cpp

---
+++ test.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//int a = 5;
+//int b = 20;
+//int ad = a + b;
+//int mu = a * b;
+//int su = a - b;
+//int di = b / a; 
+
+  //cout << a << " + " << b << " = " << ad << endl;
+  //cout << a << " * " << b << " = " << mu << endl;
+  //cout << a << " - " << b << " = " << su << endl;
+  //cout << b << " / " << a << " = " << di << endl;
+
+
+
+ // int priNum, area, perimet;  
+ // cout << "Entre um lado do quadrado: ";
+ // cin >> priNum 
+ // area = priNum * priNum;
+ // perimet = (priNum*4);
+ // cout << " O perimetro do quadrado é = " << perimet << endl ;     
+ // cout << " A area do quadrado é = " << area << endl ;    
+
+
+
+ //  int priNum, segNum, areaT;  
+ // cout << "Entre a base e altura do triangulo: ";
+ // cin >> priNum >> segNum ;
+ // areaT = (priNum * segNum) / 2 ;
+ //  cout << " A area do triangulo é = " << areaT << endl ;   
+
+
+  return 0;
+}
